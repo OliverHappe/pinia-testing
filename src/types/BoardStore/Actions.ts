058ff135b06359394bfd631bba7bef2aaa376237
Oliver Happe <oliver.happe@dataport.de>
2024-02-29 14:59:52 +0100

    adds factory for creating actions

--- src/types/BoardStore/Actions.ts
+++ src/types/BoardStore/Actions.ts
@@ -1,29 +1,56 @@
-export declare type AnyAction =
-  | UpdateCardAction
-  | DeleteCardAction
-  | LockCardAction
-  | UnlockCardAction;
+import { useBoardStore } from "@/stores/BoardStore";
+import { createAction } from "@/types/ActionFactory";
 
-interface BaseAction {
+export declare type AnyBoardAction =
+  | IUpdateCardAction
+  | IDeleteCardAction
+  | ILockCardAction
+  | CreateCardResponse
+  | IUnlockCardAction;
+
+declare type BoardActions =
+  | "update-card"
+  | "delete-card"
+  | "lock-card"
+  | "unlock-card"
+  | "no-payload-action";
+
+export const UpdateCardAction = createAction<IUpdateCardAction>("update-card");
+export const DeleteCardAction = createAction<IDeleteCardAction>("delete-card");
+export const LockCardAction = createAction<ILockCardAction>("lock-card");
+export const UnlockCardAction = createAction<IUnlockCardAction>("unlock-card");
+export const NoPayloadAction =
+  createAction<INoPayloadAction>("no-payload-action");
+export const CreateCardAction = createAction<ICreateCardAction>("");
+export const CreateCardActionSuccess = createAction<CreateCardResponse>("");
+export const CreateCardActionFail = createAction<>("");
+
+useBoardStore.dispatch(CreateCardAction);
+export interface BaseAction {
   type: string;
+  payload?: any;
 }
 
-export interface UpdateCardAction extends BaseAction {
+interface IUpdateCardAction extends BaseAction {
   type: "update-card";
   payload: { id: string; text: string };
 }
 
-export interface DeleteCardAction extends BaseAction {
+interface IDeleteCardAction extends BaseAction {
   type: "delete-card";
   payload: { id: string };
 }
 
-export interface LockCardAction extends BaseAction {
+export interface ILockCardAction extends BaseAction {
   type: "lock-card";
   payload: { id: string; userId: string };
 }
 
-export interface UnlockCardAction extends BaseAction {
+export interface IUnlockCardAction extends BaseAction {
   type: "unlock-card";
   payload: { id: string };
+}
+
+export interface INoPayloadAction extends BaseAction {
+  type: "no-payload-action";
 }
