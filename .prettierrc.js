6cad0fff3defa3fada399ed88743d652bcc3b1ef
hoeppner-dataport <marcus.hoeppner@dataport.de>
2024-02-29 15:16:31 +0100

    chore: update prettier-config

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  printWidth: 120,
+  trailingComma: "es5",
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: "always",
+  proseWrap: "never",
+  htmlWhitespaceSensitivity: "css",
+  endOfLine: "lf",
+  semi: true,
+  singleQuote: false,
+};
