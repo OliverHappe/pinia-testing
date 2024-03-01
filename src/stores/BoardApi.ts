b680d501213135e2a5969f52361d20fb49a1858f
hoeppner-dataport <marcus.hoeppner@dataport.de>
2024-03-01 16:07:13 +0100

    add path to websocket-connection

--- src/stores/BoardApi.ts
+++ src/stores/BoardApi.ts
@@ -3,7 +3,11 @@ import { io } from "socket.io-client";
 import { onUnmounted } from "vue";
 
 export const useBoardApi = (dispatch: (action: AnyBoardAction) => void) => {
-  const socket = io("http://localhost:3000");
+  const socket = io(
+    "https://bc-6683-poc-board-collaboration-server.dbc.dbildungscloud.dev",
+    // "http://localhost:3000",
+    { path: "/poc-board-collaboration-server" }
+  );
   // listen for new messages
   socket.on("connect", function () {
     console.log("connected");
